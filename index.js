c604d6707d9e00564bbbe2ac6af4df9b927434c6
Sylvain <76697214+git504@users.noreply.github.com>
2021-06-07 10:01:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let string = "Chaine";
+let number = 25;
+let boolean = true;
+let maVariable; //undefined
+
+let array = ["Bordeaux", "Toulouse", "Nante"];
+//console.log(array[0][3]);
+//[] tableau
+//{} objet
+
+let array2 = ["Bordeaux", 24, true, [1, 2], { nom: "Denis" }];
+// console.log(array2[4].nom);
+
+let objet = {
+  pseudo: "Denis",
+  age: 33,
+  technos: ["Javascript", "React", "NodeJs"],
+  admin: false,
+};
+
+// objet.adresse = "22 rue du code";
+// ---> ajoute ds objet l'index adress
+// console.log(objet);
+
+
